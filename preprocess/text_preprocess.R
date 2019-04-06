a6fe2b39c1edd04f362107e639a005dcfcf82c9a
Harvey <harveyj1994@gmail.com>
2019-04-06 14:59:06 -0400

    ssss

--- preprocess/text_preprocess.R
+++ preprocess/text_preprocess.R
@@ -20,7 +20,7 @@ input2 <- readLines(con2, 500000L)
 business <- as.data.frame(t(sapply(input2,fromJSON)))
 row.names(business) <- seq(1, nrow(business))
 
-keeps_business <- c("business_id", "name", "city", "stars", "review_count", "categories")
+keeps_business <- c("business_id",  "stars", "categories")
 business = business[keeps_business]
 
 business$is_restaurant = grepl("Restaurants", business$categories)
@@ -28,13 +28,13 @@ business = subset(business, is_restaurant == TRUE)
 business[144:147,]
 restaurant_reviews <- reviews[which(reviews$business_id %in% business$business_id),]
 
-restaurant_reviews = subset(restaurant_reviews, stars != 3)
-restaurant_reviews$positive = as.factor(restaurant_reviews$stars > 3)
+#restaurant_reviews = subset(restaurant_reviews, stars != 3)
+#restaurant_reviews$positive = as.factor(restaurant_reviews$stars > 3)
 
 restaurant_reviews$language = textcat(restaurant_reviews$text)
 restaurant_reviews = subset(restaurant_reviews, language =='english')
 
 restaurant_reviews$business_id = NULL
-write.csv(restaurant_reviews, file = "restuarant_review.csv",row.names=FALSE)
+write.csv(restaurant_reviews, file = "restuarant_review_raw_5_labels.csv",row.names=FALSE)
 
-restaurant_reviews$stars = NULL
+#restaurant_reviews$stars = NULL
