e499b7aec2b899953d52057bc2d657bde3348c6c
Zhezhong Jiang <zzjiang@scc1.bu.edu>
2019-04-12 14:23:37 -0400

    s

--- balanced_model/5_label/yoon_kim_deep_pre.py
+++ balanced_model/5_label/yoon_kim_deep_pre.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import pickle 
+import pickle
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense , Input , LSTM , Embedding, Dropout , Activation, GRU, Flatten,Conv2D,Conv1D,MaxPooling1D, Dropout,RepeatVector,Permute,merge,Lambda,multiply
+from keras.layers import Dense , Input , LSTM , Embedding, Dropout , Activation, GRU, Flatten,Conv2D,Conv1D,MaxPooling1D, Dropout
 from keras.layers import concatenate
 from keras.layers import Bidirectional, GlobalMaxPool1D,BatchNormalization
 from keras.models import Model, Sequential
@@ -14,7 +14,6 @@ import sys
 import os
 import tensorflow as tf
 import keras.backend.tensorflow_backend as ktf
-from keras import backend as K
 
 # Get the number of cores assigned to this job.
 def get_n_cores():
@@ -45,11 +44,19 @@ def get_session():
 # Assign the configured Tensorflow session to keras
 ktf.set_session(get_session()) 
 # Rest of your Keras script starts here....
+print("finished importing")
 
 
-df = pd.read_csv("/usr4/cs542sp/zzjiang/Data/restuarant_review_5_label_unbalanced.csv")
-train = df.sample(frac = 0.8,random_state = 200)
-test = df.drop(train.index)
+#######################################################################
+# Read in Data and tokenize , prepare training data and test data
+#df = pd.read_csv("C:/Users/Harvey/Desktop/Yelp_data_set/restuarant_review_5_label_unbalanced.csv")
+#df = pd.read_csv("/home/ec2-user/Data/restuarant_review_5_label_unbalanced.csv")
+
+
+
+train = pd.read_csv("/usr4/cs542sp/zzjiang/Data/restuarant_balance_5_train.csv")
+test = pd.read_csv("/usr4/cs542sp/zzjiang/Data/restuarant_balance_5_test.csv")
+
 train.loc[:,'stars'] -= 1
 print(np.unique(train['stars']))
 
@@ -74,10 +81,10 @@ test.loc[:,'stars'] -=1
 y_test = test['stars']
 print(np.unique(test['stars']))
 
-word_index = tokenizer.word_index
-embed_size = 128
+#######################################################################
 
-'''
+
+
 #####################################################################
 # Using pretrained glove vector
 #####################################################################
@@ -94,7 +101,8 @@ f.close()
 print('Total %s word vectors in Glove 6B 100d.' % len(embeddings_index))
 
 
-
+word_index = tokenizer.word_index
+embed_size = 100 
 embedding_matrix = np.random.random((len(word_index) + 1, embed_size))
 for word, i in word_index.items():
     embedding_vector = embeddings_index.get(word)
@@ -102,71 +110,73 @@ for word, i in word_index.items():
         # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
 
-print("finished embeeding matrix computation")   
-
+      
 embedding_layer = Embedding(len(word_index) + 1,
                             embed_size,
                             weights=[embedding_matrix],
                             input_length=maxlen,
-                            trainable=False)
-
-'''
-
-#Randomly initialized 
-embedding_layer = Embedding(len(word_index) + 1,
-                            embed_size,
-                            input_length=maxlen,
                             trainable=True)
 
-print("start building model") 
+#Randomly initialized 
+#embedding_layer = Embedding(len(word_index) + 1,
+#                            embed_size,
+#                            input_length=maxlen,
+#                            trainable=False)
+
+
+
+
+convs = []
+filter_sizes = [3,4,5]
+
 sequence_input = Input(shape=(maxlen,), dtype='int32')
 embedded_sequences = embedding_layer(sequence_input)
 
-units = 64
-activations = Bidirectional(LSTM(64, return_sequences = True,kernel_regularizer=regularizers.l2(0.1)))(embedded_sequences)
 
-# compute importance for each step
-attention = Dense(1, activation='tanh')(activations)
-attention = Flatten()(attention)
-attention = Activation('softmax')(attention)
-attention = RepeatVector(units*2)(attention)
-attention = Permute([2, 1])(attention)
+for fsz in filter_sizes:
+    l_conv = Conv1D(nb_filter=128,filter_length=fsz,activation='relu')(embedded_sequences)
+    l_pool = MaxPooling1D(4)(l_conv)
+    convs.append(l_pool)
+   
+l_merge = concatenate(convs,axis=1) 
+l_cov1= Conv1D(128, 5, activation='relu')(l_merge)
+l_pool1 = MaxPooling1D(4)(l_cov1)
+l_cov2 = Conv1D(128, 5, activation='relu')(l_pool1)
+l_pool2 = MaxPooling1D(4)(l_cov2)
+l_flat = Flatten()(l_pool2)
+l_dense = Dense(256, activation='relu')(l_flat)
+preds = Dense(5, activation='softmax')(l_dense)
+
+model = Model(sequence_input, preds)
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['acc'])
 
 
-sent_representation = multiply([activations, attention])
-sent_representation = Lambda(lambda xin: K.sum(xin, axis=-2), output_shape=(units*2,))(sent_representation)
-
-out = Dense(5, activation='softmax')(sent_representation)
-
-model = Model(inputs=sequence_input, outputs=out)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
 model.summary()
+
 
 batch_size = 512
 epochs = 100
 history = model.fit(X_t,y, batch_size=batch_size, epochs=epochs, validation_split=0.2)
 
+#################################################################
+#Save train history as dict 
+#################################################################
+
+with open(r"/usr4/cs542sp/zzjiang/History/Yoon_kim_deep_pre", "wb") as output_file:
+    pickle.dump(history.history, output_file)
+
+
+
 
 prediction = model.predict(X_test)
 y_pred = np.argmax(prediction,axis = 1)
 y_test = np.array(y_test)
+print(prediction[:10])
 print(y_pred[:10])
 print(y_test.shape)
 print(y_pred.shape)
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score, confusion_matrix
 print('accuracy :{0}'.format(accuracy_score(y_pred, y_test)))
-
-#################################################################
-#Save train history as dict 
-#################################################################
-
-with open(r"/usr4/cs542sp/zzjiang/History/rnn_5_rand", "wb") as output_file:
-    pickle.dump(history.history, output_file)
-
-
-
-
-
-
-
